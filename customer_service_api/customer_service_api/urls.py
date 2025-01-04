e20b18b41fc58b70cce65dc3810ff27baa4612de
Barry <barryyu0129@gmail.com>
2025-01-04 23:08:36 +0800

    add redirect

--- customer_service_api/customer_service_api/urls.py
+++ customer_service_api/customer_service_api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -25,4 +26,5 @@ urlpatterns = [
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', RedirectView.as_view(url='/api/schema/swagger-ui/', permanent=False), name='api-docs'), #redirect to swagger ui
 ]
