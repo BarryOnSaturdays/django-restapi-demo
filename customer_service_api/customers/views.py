6ec175bebd5320004076ae60f0b0cf01d691c7e2
Barry <barryyu0129@gmail.com>
2025-01-04 22:28:34 +0800

    authentication

--- customer_service_api/customers/views.py
+++ customer_service_api/customers/views.py
@@ -1,19 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Customer, ServiceInteraction
 from .serializers import CustomerSerializer, ServiceInteractionSerializer
 
 class CustomerListCreate(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class CustomerRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class InteractionListCreate(generics.ListCreateAPIView):
     queryset = ServiceInteraction.objects.all()
     serializer_class = ServiceInteractionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class InteractionRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = ServiceInteraction.objects.all()
     serializer_class = ServiceInteractionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
